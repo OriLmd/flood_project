244de050811688a6908c7ead5061cc02b7e8730e
Charles_Etienne_Mariotte <charlesetienne.mariotte@gmail.com>
2023-03-07 10:01:57 +0100

    Updated name 'toto' -> 'flood_project'

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content]
 
-setup(name='toto',
+setup(name='flood_project',
       description="package description",
-      packages=find_packages(), # NEW: find packages automatically
-      install_requires=requirements) # NEW
+      packages=find_packages(),
+      install_requires=requirements)
