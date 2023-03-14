d1701923ea878286554d0122748cc12ae792e069
kartik varma <gobarsinghji@gmail.com>
2023-03-14 16:55:42 +0100

    added new files, fast, stream_litt, changed requirements

--- api/fast.py
+++ api/fast.py
@@ -2,82 +2,21 @@ import numpy as np
 import pandas as pd
 import cv2
 import matplotlib.pyplot as plt
-# $WIPE_BEGIN
-
-#from taxifare.ml_logic.registry import load_model
-#from taxifare.ml_logic.preprocessor import preprocess_features
-# $WIPE_END
-
+import io
 from fastapi import FastAPI, File, UploadFile
 from typing import List
 from starlette.responses import Response
-from tensorflow import convert_to_tensor
+from tensorflow import convert_to_tensor, concat, io, data
 from tensorflow.keras import models
 from PIL import Image
-
-# Internal imports
+#from ml_logic.load_preprocess import read_one_image
 from ml_logic import metrics
+from ml_logic.results import result_into_class
 
-# from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
 
-## Optional, good practice for dev purposes. Allow all middlewares
-#app.add_middleware(
-#    CORSMiddleware,
-#    allow_origins=["*"],  # Allows all origins
-#    allow_credentials=True,
-#    allow_methods=["*"],  # Allows all methods
-#    allow_headers=["*"],  # Allows all headers
-#)
-
-# $WIPE_BEGIN
-#💡 Preload the model to accelerate the predictions
-# We want to avoid loading the heavy deep-learning model from MLflow at each `get("/predict")`
-# The trick is to load the model in memory when the uvicorn server starts
-# Then to store the model in an `app.state.model` global variable accessible across all routes!
-# This will prove very useful for demo days
-#app.state.model = load_model()
-#$WIPE_END
-
-# http://127.0.0.1:8000/predict?pickup_datetime=2012-10-06 12:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2
-#@app.get("/predict")
-#def predict(pickup_datetime: str,  # 2013-07-06 17:18:00
-#            pickup_longitude: float,    # -73.950655
-#            pickup_latitude: float,     # 40.783282
-#            dropoff_longitude: float,   # -73.984365
-#            dropoff_latitude: float,    # 40.769802
-#            passenger_count: int):      # 1
-#    """
-#    Make a single course prediction.
-#    Assumes `pickup_datetime` is provided as string by the user in "%Y-%m-%d %H:%M:%S" format
-#    Assumes `pickup_datetime` implicitely refers to "US/Eastern" timezone (as any user in New York City would naturally write)
-#    """
-#    # $CHA_BEGIN
-#
-#    # Convert to US/Eastern TZ-aware!
-#    pickup_datetime_localized = pd.Timestamp(pickup_datetime, tz='US/Eastern')
-#
-#    X_pred = pd.DataFrame(dict(
-#        pickup_datetime=[pickup_datetime_localized],
-#        pickup_longitude=[pickup_longitude],
-#        pickup_latitude=[pickup_latitude],
-#        dropoff_longitude=[dropoff_longitude],
-#        dropoff_latitude=[dropoff_latitude],
-#        passenger_count=[passenger_count]))
-#
-#    model = app.state.model
-#    assert model is not None
-#
-#    X_processed = preprocess_features(X_pred)
-#    y_pred = model.predict(X_processed)
-#
-#    # ⚠️ fastapi only accepts simple python data types as a return value
-#    # among which dict, list, str, int, float, bool
-#    # in order to be able to convert the api response to json
-#    return dict(fare_amount=float(y_pred))
-#    # $CHA_END
-#
 
 @app.get("/")
 def root():
@@ -85,86 +24,34 @@ def root():
     return dict(greeting="Hello")
     # $CHA_END
 
-# Good one
+
 @app.post("/upload")
-async def receive_image(images: List[UploadFile]=File(...)):
+async def receive_image(files: List[UploadFile]=File(...)):
+
     images_list = []
-    ### Receiving and decoding the image
-    for img in images:
+
+    # Receiving and decoding the image
+    for img in files:
         contents = await img.read()
-        nparr = np.fromstring(contents, np.uint8)
-        img_gs = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        img_gs_norm = img_gs/255
+        #contents = io.read_file(img)
+        img_one_channel = io.decode_png(contents, channels=1) # channels=1 to have a grayscale image
+        img_norm = img_one_channel/255
 
-        images_list.append(img_gs_norm) # extension depends on which format is sent from Streamlit
+        images_list.append(img_norm)
 
-    for_test = convert_to_tensor(images_list)
-    model_test = models.load_model('/Users/mariottecharles-etienne/code/OriLmd/flood_project/models/first_unet_2000_20230310-143412.h5',
-                                 custom_objects={"DiceLoss": metrics.DiceLoss(), "Dice":metrics.Dice(), 'TotalError':metrics.TotalError()})
-    img_pred = model_test.predict(for_test)
-    im_bytes = cv2.imencode('.png', img_pred[0])[1].tobytes()
+    img_for_test = concat(images_list,axis=-1)
+
+    # Load the model
+    model_test = models.load_model('model_for_api.h5',
+                                 custom_objects={"DiceLoss": metrics.DiceLoss(),"Dice":metrics.Dice(), 'TotalError':metrics.TotalError()})
+
+    # Predict
+    img_pred = model_test.predict(np.expand_dims(img_for_test,axis=0))
+
+    #Choose according to threshold
+    img_pred_threshold = result_into_class(img_pred)*255
+
+    # Transform predicted image to bytes in order to display it
+    im_bytes = cv2.imencode('.png', img_pred_threshold[0])[1].tobytes()
 
     return Response(content=im_bytes, media_type="image/png")
-
-# @app.post("/upload")
-# async def receive_image(images: List[UploadFile]=File(...)):
-
-#     images_list = []
-#     ### Receiving and decoding the image
-#     for img in images:
-#         contents = await img.read()
-#         nparr = np.fromstring(contents, np.uint8)
-#         cv2_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # type(cv2_img) => numpy.ndarray
-
-#         ### Encoding and responding with the image
-#         images_list.append(cv2.imencode('.png', cv2_img)[1]) # extension depends on which format is sent from Streamlit
-
-#     return Response(content=images_list[1].tobytes(), media_type="image/png")
-
-# @app.post("/upload")
-# async def receive_image(images: List[UploadFile]=File(...)):
-#     images_list = []
-#     ### Receiving and decoding the image
-#     for img in images:
-#         contents = await img.read()
-#         nparr = np.fromstring(contents, np.uint8)
-#         img_gs = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-#         img_gs_norm = img_gs/255
-
-#         images_list.append(img_gs_norm) # extension depends on which format is sent from Streamlit
-
-#     for_test = convert_to_tensor(images_list)
-#     model_test = models.load_model('/Users/mariottecharles-etienne/code/OriLmd/flood_project/models/first_unet_2OOO_v2_20230310-171824.h5',
-#                                  custom_objects={"DiceLoss": metrics.DiceLoss(), "Dice":metrics.Dice(), 'TotalError':metrics.TotalError()})
-#     img_pred = model_test.predict(for_test)
-#     # im = cv2.imencode('.png', img_pred)[1]
-
-#     return Response(content=img_pred[1].tobytes(), media_type="image/png")
-#     # return {'type':str(type(img_pred)),'shape':img_pred.shape}
-
-# @app.post("/upload")
-# async def receive_image(images: List[UploadFile]=File(...)):
-#     images_list = []
-#     ### Receiving and decoding the image
-#     for img in images:
-#         contents = await img.read()
-
-#         nparr = np.fromstring(contents, np.uint8)
-#         cv2_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-#         img_gs_norm = cv2_img/255
-#         #nparr = np.fromstring(contents, np.uint8)
-#         #cv2_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # type(cv2_img) => numpy.ndarray
-#         ### Encoding and responding with the image
-#         images_list.append(img_gs_norm) # extension depends on which format is sent from Streamlit
-
-#     for_test = convert_to_tensor(images_list)
-#     model_test = models.load_model("/Users/mariottecharles-etienne/code/OriLmd/flood_project/models/first_unet_2OOO_v2_20230310-171824.h5",
-#                                    custom_objects = {"DiceLoss": metrics.DiceLoss(), "Dice":metrics.Dice(), 'TotalError':metrics.TotalError()})
-
-#     img_pred = model_test.predict(for_test)
-#     #im = Image.fromarray(img_pred)
-#     #im.save('pred.png')
-#     #nparr_pred = np.fromstring(img_pred, np.uint8)
-#     #cv2_img_pred = cv2.imread(nparr_pred, cv2.IMREAD_COLOR)
-#     return Response(content=img_pred[1,:,:,0].tobytes(), media_type="image/png")
-#     #return Response(content=im.tobytes(), media_type="image/png")
--- api/stream_litt.py
+++ api/stream_litt.py
@@ -1,43 +1,66 @@
 import streamlit as st
+from PIL import Image
 import requests
-import json
+#from dotenv import load_dotenv
+import os
 import numpy as np
-from io import BytesIO
 
-st.title("We can make a flood mask")
+# Set page tab display
+st.set_page_config(
+   page_title="Flood Detection using U-Net",
+   page_icon= '🌊',
+   layout="wide",
+   initial_sidebar_state="expanded",
+)
 
-option = st.selectbox('What operation',
-                      ('flood','not flood'))
-
-#st.write("")
-st.write("How much flood do you want?")
-x = st.slider("Flood", 0,100,30)
-
-inputs = {"flood":option , "x":x}
-
-uploaded_files = st.file_uploader("Upload images", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-
-    #x = np.arange(28*28).reshape(28, 28)
-
-    # save in to BytesIo buffer
-    np_bytes = BytesIO()
-    np.save(np_bytes, bytes_data, allow_pickle=True)
-
-    # get bytes value
-    np_bytes = np_bytes.getvalue()
-
-    # load from bytes into numpy array
-    load_bytes = BytesIO(np_bytes)
-    loaded_np = np.load(load_bytes, allow_pickle=True)
-
-    st.write("filename:", uploaded_file.name)
-    st.write(np_bytes)
-    st.write(loaded_np)
+# Example local Docker container URL
+# url = 'http://api:8000'
+# Example localhost development URL
+url = 'http://localhost:8000'
+#load_dotenv()
+#url = os.getenv('API_URL')
 
 
+# App title and description
+st.header('🌊 Flood Detection using U-Net 🌊')
 
-if st.button('Generate Flood'):
-    res = requests.post(url = "http://127.0.0.1:8000/test", data = json.dumps(inputs))
-st.subheader(f"Does our model make a flood? = {res.text}")
+
+### Create a native Streamlit file upload input
+st.subheader("Upload your images and see what the results are with a flood👇")
+
+#upload vv,vh and wb images
+cola, colb, colc = st.columns(3)
+with cola:
+    img_vv = st.file_uploader('Upload vv image', type = 'png', accept_multiple_files=False)
+with colb:
+    img_vh = st.file_uploader('Upload vh image', type = 'png', accept_multiple_files=False)
+    st.write('')
+with colc:
+    img_wb = st.file_uploader('Upload wb image', type = 'png', accept_multiple_files=False)
+
+
+#once images are uploaded send for prediction
+
+if st.button('Get Prediction'):
+    img_file_buffer = [img_vv,img_vh,img_wb]
+    image_data = []
+    for image in img_file_buffer:
+        image_data.append(('files',image))
+    res = requests.post(url + "/upload", files = image_data)
+
+    #this is for centering the image
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.write(' ')
+
+    with col2:
+        st.image(res.content)
+
+    with col3:
+        st.write(' ')
+    #adding text below image to describe the image
+    st.subheader("Here is what this region looks like if it were flooded ☝️")
+else:
+    #while images aren't uploaded it will display this messages
+    st.write('🚨 Click this button only when images are uploaded 🚨')
